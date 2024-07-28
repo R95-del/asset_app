c57c7081c24e222382fc3c4d44e02e57af744f69
rishi <somani.rishi81@gmail.com>
2024-07-28 22:50:25 +0530

    Added Check box for admin login, contoller, error show

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,22 +1,28 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
   #   super
   # end
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+# POST /resource/sign_in
+  def create
+    super do |resource|
+      if params[:user][:admin] == '1' && !resource.admin?
+        sign_out resource
+        flash[:alert] = "You are not authorized to log in as admin."
+        redirect_to new_user_session_path and return
+      end
+    end
+  end
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
   end
-  
+
   def after_sign_in_path_for(resource_or_scope)
     stored_location_for(resource_or_scope) || root_path
   end
@@ -29,7 +35,7 @@ class Users::SessionsController < Devise::SessionsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:admin])
+  end
 end
